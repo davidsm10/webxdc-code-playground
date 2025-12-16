bb2adb1902ab626d3a4fede4ebfe2ef9bc9d91c9
WofWca <wofwca@protonmail.com>
2025-12-16 13:04:08 +0400

    improvement: add links to spec in HTML and JS

--- src/public/template/index.js
+++ src/public/template/index.js
@@ -3,11 +3,13 @@ const input = document.getElementById("input");
 const form = document.getElementById("form");
 const output = document.getElementById("output");
 
+// https://webxdc.org/docs/spec/selfAddr_and_selfName.html
 device.innerText = "This is " + window.webxdc.selfName;
 
 function sendMsg(e) {
   e.preventDefault();
   const msg = window.webxdc.selfName + ": " + input.value;
+  // https://webxdc.org/docs/spec/sendUpdate.html#sendupdate
   window.webxdc.sendUpdate({ payload: msg, info: msg }, "");
   input.value = "";
 }
@@ -16,4 +18,5 @@ form.onsubmit = sendMsg;
 function receiveUpdate(update) {
   output.innerText += update.payload + "\n";
 }
+// https://webxdc.org/docs/spec/setUpdateListener.html
 window.webxdc.setUpdateListener(receiveUpdate, 0);
