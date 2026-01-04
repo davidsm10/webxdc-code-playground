6982c141b6b62ab2b374bf87199a30863846c091
davidsm10 <davidsm10@systemli.org>
2026-01-04 09:26:56 -0500

    refactor: move some code into `app.ts`

--- src/App.svelte
+++ src/App.svelte
@@ -5,54 +5,20 @@
   import FileManager from "./components/FileManager/FileManager.svelte";
   import { openTabs, activeTab } from "./components/Tabs/state.svelte";
   import { FilesIcon, PlayIcon, Share2Icon } from "@lucide/svelte";
-  import JSZip from "jszip";
   import { configureSingle } from "@zenfs/core";
-  import { resolve } from "@zenfs/core/path";
   import { IndexedDB } from "@zenfs/dom";
-  import { readdir, readFile, writeFile } from "@zenfs/core/promises";
   import { wrap } from "comlink";
   import type { WorkerShape } from "@valtown/codemirror-ts/worker";
+  import { setupTemplate, exportWebxdc } from "./app";
 
   const rawTypescriptWorker = new Worker(
     new URL("./typescript/worker.ts", import.meta.url),
     {
       type: "module",
-    }
+    },
   );
   const typescriptWorker = wrap<WorkerShape>(rawTypescriptWorker);
   typescriptWorker.initialize();
-
-  async function setupTemplate() {
-    if (!(await readdir("/")).length) {
-      const list: string[] = await (await fetch("/template.json")).json();
-      for (const path of list) {
-        const content = await (await fetch("/template" + path)).text();
-        await writeFile(path, content);
-      }
-    }
-  }
-
-  async function exportWebxdc() {
-    const zip = new JSZip();
-    const files = await readdir("/", { withFileTypes: true, recursive: true });
-    for (const file of files) {
-      if (file.isDirectory()) continue;
-      const path = resolve(file.parentPath, file.name);
-      const content = await readFile(path);
-      zip.file(path.replace("/", ""), content);
-    }
-    const zipBlob = await zip.generateAsync({
-      type: "blob",
-      compression: "DEFLATE",
-      compressionOptions: { level: 9 },
-    });
-    window.webxdc.sendToChat({
-      file: {
-        name: "app.xdc",
-        blob: zipBlob,
-      },
-    });
-  }
 </script>
 
 <div class="container">
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,40 @@
 import localforage from "localforage";
+import { readdir, readFile, writeFile } from "@zenfs/core/promises";
+import JSZip from "jszip";
+import { resolve } from "@zenfs/core/path";
 
 export const generalDB = localforage.createInstance({
   name: "code-playground-general-database",
 });
+
+export async function setupTemplate() {
+  if (!(await readdir("/")).length) {
+    const list: string[] = await (await fetch("/template.json")).json();
+    for (const path of list) {
+      const content = await (await fetch("/template" + path)).text();
+      await writeFile(path, content);
+    }
+  }
+}
+
+export async function exportWebxdc() {
+  const zip = new JSZip();
+  const files = await readdir("/", { withFileTypes: true, recursive: true });
+  for (const file of files) {
+    if (file.isDirectory()) continue;
+    const path = resolve(file.parentPath, file.name);
+    const content = await readFile(path);
+    zip.file(path.replace("/", ""), content);
+  }
+  const zipBlob = await zip.generateAsync({
+    type: "blob",
+    compression: "DEFLATE",
+    compressionOptions: { level: 9 },
+  });
+  window.webxdc.sendToChat({
+    file: {
+      name: "app.xdc",
+      blob: zipBlob,
+    },
+  });
+}
