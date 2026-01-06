0b005d3e9d1df782ea5d59fd905491545eecba88
davidsm10 <davidsm10@systemli.org>
2026-01-06 11:09:23 -0500

    refactor: move editor extensions code into extensions.ts

--- src/components/Editor/Editor.svelte
+++ src/components/Editor/Editor.svelte
@@ -1,54 +1,14 @@
 <script lang="ts">
   import CodeMirror from "svelte-codemirror-editor";
-  import { basicSetup, EditorView } from "codemirror";
-  import { autocompletion } from "@codemirror/autocomplete";
-  import { oneDark } from "@codemirror/theme-one-dark";
-  import { html } from "@codemirror/lang-html";
-  import { css } from "@codemirror/lang-css";
-  import { javascript } from "@codemirror/lang-javascript";
-  import { json } from "@codemirror/lang-json";
+  import { EditorView } from "codemirror";
+  import { generalExtensions, getLanguageExtensions } from "./extensions";
   import { writeFile, readFile } from "@zenfs/core/promises";
   import { type WorkerShape } from "@valtown/codemirror-ts/worker";
-  import {
-    tsFacetWorker,
-    tsSyncWorker,
-    tsLinterWorker,
-    tsAutocompleteWorker,
-    tsHoverWorker,
-  } from "@valtown/codemirror-ts";
-  import { jsCompletions } from "./js-completions";
 
   let {
     path,
     typescriptWorker,
   }: { path: string; typescriptWorker: WorkerShape } = $props();
-
-  async function getExtensions() {
-    if (path.endsWith(".js")) {
-      return [
-        basicSetup,
-        javascript(),
-        tsFacetWorker.of({
-          worker: typescriptWorker,
-          path: path.replace("/", ""),
-        }),
-        tsSyncWorker(),
-        tsLinterWorker(),
-        autocompletion({
-          override: [tsAutocompleteWorker(), jsCompletions],
-        }),
-        tsHoverWorker(),
-      ];
-    } else if (path.endsWith(".html")) {
-      return [basicSetup, html()];
-    } else if (path.endsWith(".css")) {
-      return [basicSetup, css()];
-    } else if (path.endsWith(".json")) {
-      return [basicSetup, json()];
-    }
-
-    return [basicSetup];
-  }
 
   async function onReady(view: EditorView) {
     view.contentDOM.setAttribute("spellcheck", "false");
@@ -59,18 +19,17 @@
 </script>
 
 {#await readFile(path, { encoding: "utf-8" }) then value}
-  {#await getExtensions() then extensions}
-    <CodeMirror
-      styles={{
-        "&": { height: "100%" },
-        ".cm-scroller": { "overflow-y": "auto" },
-      }}
-      {extensions}
-      lineWrapping={true}
-      theme={oneDark}
-      {value}
-      onchange={(val) => writeFile(path, val)}
-      onready={onReady}
-    />
-  {/await}
+  <CodeMirror
+    styles={{
+      "&": { height: "100%" },
+      ".cm-scroller": { "overflow-y": "auto" },
+    }}
+    extensions={[
+      generalExtensions,
+      getLanguageExtensions(path, typescriptWorker),
+    ]}
+    {value}
+    onchange={(val) => writeFile(path, val)}
+    onready={onReady}
+  />
 {/await}
---
+++ src/components/Editor/extensions.ts
@@ -0,0 +1,49 @@
+import { basicSetup } from "codemirror";
+import { oneDark } from "@codemirror/theme-one-dark";
+import { EditorView } from "codemirror";
+import { autocompletion } from "@codemirror/autocomplete";
+import { html } from "@codemirror/lang-html";
+import { css } from "@codemirror/lang-css";
+import { javascript } from "@codemirror/lang-javascript";
+import { json } from "@codemirror/lang-json";
+import {
+  tsFacetWorker,
+  tsSyncWorker,
+  tsLinterWorker,
+  tsAutocompleteWorker,
+  tsHoverWorker,
+} from "@valtown/codemirror-ts";
+import { jsCompletions } from "./js-completions";
+import type { WorkerShape } from "@valtown/codemirror-ts/worker";
+
+export const generalExtensions = [basicSetup, oneDark, EditorView.lineWrapping];
+
+export function getLanguageExtensions(
+  path: string,
+  typescriptWorker: WorkerShape,
+) {
+  let extensions = [];
+  if (path.endsWith(".html")) {
+    extensions.push(html());
+  } else if (path.endsWith(".css")) {
+    extensions.push(css());
+  } else if (path.endsWith(".json")) {
+    extensions.push(json());
+  } else if (path.endsWith(".js")) {
+    extensions.push(
+      javascript(),
+      tsFacetWorker.of({
+        worker: typescriptWorker,
+        path: path,
+      }),
+      tsSyncWorker(),
+      tsLinterWorker(),
+      autocompletion({
+        override: [tsAutocompleteWorker(), jsCompletions],
+      }),
+      tsHoverWorker(),
+    );
+  }
+
+  return extensions;
+}
